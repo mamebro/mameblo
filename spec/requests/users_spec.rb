4fe17c539e0a174f141601eebe273c31beab3a2e
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-06-11 21:58:01 +0900

    Test for the password and password confirmation.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
-describe "Users" do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_path
-      response.status.should be(200)
+  describe "User pages" do
+
+    subject { page }
+
+    describe "signup page" do
+     before { visit signup_path }
+
+       it { should have_selector('h1',    text: 'Sign up') }
+       it { should have_selector('title', text: 'Sign up') }
+      end
     end
-  end
-end
