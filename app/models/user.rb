95c6f07b51be485c1278b8bc532f142dd17f5561
Yuta Kurotaki <number999666@yahoo.co.jp>
2012-06-12 08:18:07 +0900

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :password, password_confirmation
+  has_secure_password
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximam: 50}
+
+
+#  VALID_NAME_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#  validates :name, presence:   true,
+#                    format:     { with: VALID_NAME_REGEX },
+#                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,24 +13,45 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-   describe "when password is not present" do
-       before { @user.password = @user.password_confirmation = " " }
-           it { should_not be_valid }
-             end
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+      it { should_not be_valid }
+  end
 
-               describe "when password doesn't match confirmation" do
-                   before { @user.password_confirmation = "mismatch" }
-                       it { should_not be_valid }
-                         end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+      it { should_not be_valid }
+  end
 
-                           describe "when password confirmation is nil" do
-                               before { @user.password_confirmation = nil }
-                                   it { should_not be_valid }
-                 end
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+      it { should_not be_valid }
+  end  
 
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
 
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_name(@user.name) }
+  end
   
+  describe "with valid password" do
+    it { should == found_user.authenticate(@user.password) }
+  end
+
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+    it { should_not == user_for_invalid_password }
+    specify { user_for_invalid_password.should be_false }
+  end
+
+
 end
