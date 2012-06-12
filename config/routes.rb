dc8339d82e193cb56113788d22d30998ecb6ea6d
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-06-12 21:04:34 +0900

    Chenge index, routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Mameblog::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  match '/signup',  to: 'users#new'
-  match '/signin',  to: 'sessions#new'
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
