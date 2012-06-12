4edc54b6790af99ebbf2c87939c0ac786dbfd0ef
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-06-12 19:40:30 +0900

    Change config/routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
   def destroy
   end
 end
---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe "AuthenticationPages" do
+
+  subject { page }
+
+  describe "signin page" do
+  end
+
+  describe "signin" do
+  #  before { visit signin_path }
+
+    describe "with invalid information" do
+  #    before { click_button "ログイン" }
+
+    end
+  end
+end
