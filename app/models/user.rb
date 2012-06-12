726ad74662359ebf976ae215521c827c5bc619b1
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-06-12 12:41:03 +0900

    Fixed error user pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation
   has_secure_password
 
-  validates :name, presence: true, length: {maximam: 50}
+  validates :name, presence: true, length: {maximum: 50}
 
 
-#  VALID_NAME_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#  validates :name, presence:   true,
-#                    format:     { with: VALID_NAME_REGEX },
-#                    uniqueness: { case_sensitive: false }
-#  validates :password, presence: true, length: { minimum: 6 }
-#  validates :password_confirmation, presence: true
+  VALID_NAME_REGEX = /[a-z]+\z/i
+  validates :name, presence:   true,
+                    format:     { with: VALID_NAME_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Mameblog::Application.routes.draw do
   resources :users
 
-  match '/signup',  to: 'users#new'
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
