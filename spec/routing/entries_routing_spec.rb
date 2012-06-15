190f8a330a69714f720161d2fb303da75fc78149
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-06-15 11:31:06 +0900

    fixed routes, tests & clean up

--- spec/routing/entries_routing_spec.rb
+++ spec/routing/entries_routing_spec.rb
@@ -3,14 +3,6 @@ require "spec_helper"
 describe EntriesController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/entries").should route_to("entries#index")
-    end
-
-    it "routes to #new" do
-      get("/entries/new").should route_to("entries#new")
-    end
-
     it "routes to #show" do
       get("/entries/1").should route_to("entries#show", :id => "1")
     end
