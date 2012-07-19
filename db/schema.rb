e12fb80b59ddd5b1f9b7894273e1e91bc5239826
shikakun <k@u-n.jp>
2012-07-19 23:44:10 +0900

    スタイルシートをリセット+不要なスタイルシートを削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120717164053) do
+ActiveRecord::Schema.define(:version => 20120718224804) do
 
   create_table "entries", :force => true do |t|
     t.integer  "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120717164053) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
