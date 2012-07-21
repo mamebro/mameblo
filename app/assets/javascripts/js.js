fa0b5547bd6918bd70bdb00f882674786459f801
shikakun <k@u-n.jp>
2012-07-22 04:35:32 +0900

    ブラザー関係とブログ本文の表示をモダンにした

--- app/assets/javascripts/js.js
+++ app/assets/javascripts/js.js
@@ -24,6 +24,19 @@ $(document).ready(function() {
     $('#unBrotherAfter').show();
   });
   
+  // anchor
+  $("a[href*=#]").click(function(){
+		if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+			var $target = $(this.hash);
+			$target = $target.length && $target || $("[name=' + this.hash.slice(1) +']");
+			if($target.length){
+				var targetOffset = $target.offset().top;
+				$("html,body").animate({scrollTop: targetOffset}, 500);
+				return false;
+			}
+		}
+	});
+  
   // textarea resize
   $('textarea').each(function(){
 	$(this)
