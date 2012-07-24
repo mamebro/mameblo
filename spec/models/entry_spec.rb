42b4b073aa12ffd2cc1e3a2d0d402a1fb56d0e6a
Yuta Kurotaki <number999666@yahoo.co.jp>
2012-07-25 08:15:48 +0900

    テスト修正中、まだ落ちる所たくさんあるよ

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,22 +1,49 @@
 require 'spec_helper'
 
 describe Entry do
-
+  
   let(:user) { FactoryGirl.create(:user) }
+  before { @entry = user.entry.build(content: "Lorem ipsum") }
 
   subject { @entry }
-
-  before do
-    @entry = Entry.create
-  end
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
-  
-  describe "many entries appear" do
+  its(:user) { should == user }
 
-#    FactoryGirl.create(:entry, user: @user, created_at: 1.year.ago)
-#    FactoryGirl.create(:entry, user: @user, created_at: 1.hour.ago)
+  it { should be_valid }
+
+  describe "accessible attributes" do
+    it "should not allow access to user_id" do
+      expect do
+        Entry.new(user_id: user.id)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+
+  describe "when user_id is not present" do
+    before { @entry.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with blank content" do
+    before { @entry.content = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "with content that is too long" do
+    before { @entry.content = "a" * 100000 }
+    it { should_not be_valid }
+  end
+
+  describe "with blank title" do
+    before { @entry.title = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "with content that is too long" do
+    before { @entry.title = "a" * 100000 }
+    it { should_not be_valid }
   end
 end
