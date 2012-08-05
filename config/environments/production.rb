4d49f564b632e67369a1cca2e886af84fd795b88
Yuta Kurotaki <number999666@yahoo.co.jp>
2012-08-05 15:51:46 +0900

    sslを使わずhttpで通信する
    sqaleが対応したらforce_sslをtrueにする

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+# -*- coding: undecided -*-
 Mameblog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -28,7 +29,8 @@ Mameblog::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # sqaleではまだssl対応していないのでhttpで接続するようにする。
+  config.force_ssl = false
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
