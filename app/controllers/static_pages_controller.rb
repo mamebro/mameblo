49bb9a05fefd25391a133b6285f12c0815476d54
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-09-26 18:10:39 +0900

    TwitterAPIにリクエストできなくなった時の処理を追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,11 @@ class StaticPagesController < ApplicationController
       @entry  = current_brother.entries.build
       @feed_items = current_brother.feed.paginate(page: params[:page])
     else
-      @res = Twitter.user_timeline("mameblo", {"count" => 1})
+      begin
+        @res ||= Twitter.user_timeline("mameblo", {"count" => 1})
+      rescue
+        @res = "まめぶろに来てくれてありがとう!!!"
+      end
     end
   end
 end
