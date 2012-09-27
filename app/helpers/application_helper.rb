b14ad5664a19c7ec2663c735bc300d683399199f
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-09-27 23:19:39 +0900

    使っていないメソッドの削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 module ApplicationHelper
 
-  # http://d.hatena.ne.jp/nedate/20101005/1286243852
-  module ActionView
-    class OutputBuffer < ActiveSupport::SafeBuffer
-      def <<(value)
-        super(value.to_s.force_encoding('UTF-8'))
-      end
-      alias :append= :<<
-    end
-  end
-
   #Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "まめぶろ"
