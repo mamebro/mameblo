aa8d19cced11a15f661266014833767e2bf17e19
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-10-11 05:05:46 +0900

    サーカスを見たがっているブラザー一覧を表示するようにした

--- app/controllers/circuses_controller.rb
+++ app/controllers/circuses_controller.rb
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 class CircusesController < ApplicationController
   def index
+    # 最低一回はサーカス見たい!!ボタンを押したブラザーを表示する
     participation = []
-    circuses = Circus.find(:all)
-    # circuses.each do |circus|
-    #   participation << Brother.find_by_id(circus[:brother_id])
-    # end
-    # # 重複している要素は取り除き、参加ボタンを押したブラザーをArrayで返す
-    # @participation = participation.uniq!
+    circuses = Circus.find(:all, select: 'brother_id')
+    # サーカス見たい!!ボタンを押したブラザーをArrayに入れる
+    circuses.each do |circus|
+      participation << circus[:brother_id]
+    end
+    # 重複している要素を取り除いて、サーカス見たいブラザー達のArrayを作る
+    @participate_brother = []
+    participation.uniq.each do |id|
+      @participate_brother << Brother.find(id)
+    end
+    @participate_brother
     @count = circuses.count # サーカス見たいボタンが押された数
   end
 
