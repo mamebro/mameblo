f0a1383a32565ff6b0a4b0fd677c9ebd27a3eb7c
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-10-15 06:10:54 +0900

    DRY

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -5,20 +5,20 @@ describe "Entries" do
 
   subject { page }
 
-  describe "サインインしたとき" do
+  describe "サインインして日記を投稿するとき" do
     let(:brother) { FactoryGirl.create(:brother) }
     let(:entry) { FactoryGirl.create(:entry) }
     before do
       visit root_path
       sign_in brother
-    end
-
-    it '日記が投稿できること' do
       click_link '書く'
       fill_in 'entry_title', with: '日記のタイトルです!!!'
       fill_in 'textarea', with: 'こんにちは、本文です。'
       click_button '投稿'
       redirect_to brothers_path
+    end
+
+    it '日記が投稿できること' do
       should have_content 'ぶろぐ投稿できたね'
       redirect_to brother
       should have_content 'Edit'
@@ -26,21 +26,11 @@ describe "Entries" do
     end
 
     it '日記を削除できること' do
-      click_link '書く'
-      fill_in 'entry_title', with: '日記のタイトルです!!!'
-      fill_in 'textarea', with: 'こんにちは、本文です。'
-      click_button '投稿'
-      redirect_to brothers_path
       click_link 'Delete'
       should have_content '日記を消したぜブラザー'
     end
 
     it '日記を編集できること' do
-      click_link '書く'
-      fill_in 'entry_title', with: '日記のタイトルです!!!'
-      fill_in 'textarea', with: 'こんにちは、本文です。'
-      click_button '投稿'
-      redirect_to brothers_path
       click_link 'Edit'
       click_button '投稿'
       should have_content '!!! 編集完了したね !!!'
