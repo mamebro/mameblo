98e3db8b1e7a9b706532c3520409577f3a09a0da
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-10-25 10:19:08 +0900

    Add spec for circus

--- spec/controllers/circuses_controller_spec.rb
+++ spec/controllers/circuses_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe CircusesController do
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
 
+  describe "POST 'create'" do
+    it "returns http success" do
+      post 'create'
+      response.should be_success
+    end
+  end
 end
