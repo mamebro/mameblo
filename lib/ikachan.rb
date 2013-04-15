29c537f72aa1ad06d8a43bbf0f0fed7c004d01aa
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-04-15 22:51:29 +0900

    デプロイ時にhostの設定を読み込むようにした

--- lib/ikachan.rb
+++ lib/ikachan.rb
@@ -1,14 +1,14 @@
 module Ikachan
   def ikachan_join
     config = YAML.load_file(Rails.root.to_s + '/config/irc.yml')
-    Net::HTTP.post_form(URI.parse(config['host'] + 'join'), {
+    Net::HTTP.post_form(URI.parse(config['host'] + '/join'), {
       'channel' => '#mameblo',
     })
   end
 
   def ikachan_post(message)
     config = YAML.load_file(Rails.root.to_s + '/config/irc.yml')
-    Net::HTTP.post_form(URI.parse(config['host'] + 'privmsg'), {
+    Net::HTTP.post_form(URI.parse(config['host'] + '/privmsg'), {
       'channel' => '#mameblo',
       'message' => message,
     })
