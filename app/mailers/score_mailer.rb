7c80d0c6f37e4f838232ea0ff1f317e4797ef86a
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-07-01 09:56:55 +0900

    invalid multibyte char (US-ASCII) でエラーになるので、コメントアウトした

--- app/mailers/score_mailer.rb
+++ app/mailers/score_mailer.rb
@@ -1,13 +1,13 @@
 class ScoreMailer < ActionMailer::Base
-  default from: "example@mameblo.com"
-
-  def daily
-    @date          = Time.now.yesterday.strftime('%Y年%m月%d日')
-    @brother_total = Brother.count
-    @entry_total   = Entry.count
-    @circus_total  = Circus.count
-    mail(to:      ["yuta.kurotaki@gmail.com"],
-         # cc:      [""],
-         subject: "【まめぶろ】Dailyスコアレポート #{@date}")
-  end
+#  default from: "example@mameblo.com"
+#
+#  def daily
+#    @date          = Time.now.yesterday.strftime('%Y年%m月%d日')
+#    @brother_total = Brother.count
+#    @entry_total   = Entry.count
+#    @circus_total  = Circus.count
+#    mail(to:      ["yuta.kurotaki@gmail.com"],
+#         # cc:      [""],
+#         subject: "【まめぶろ】Dailyスコアレポート #{@date}")
+#  end
 end
