d0f4257b9a8bcefc12043eb927d9fffac3250c2d
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-09-11 04:11:46 +0900

    スタッフのメールアドレスはymlで管理する

--- app/mailers/score_mailer.rb
+++ app/mailers/score_mailer.rb
@@ -2,12 +2,12 @@ class ScoreMailer < ActionMailer::Base
   default from: "score@mamebro.com"
 
   def daily
-    @date          = Time.now.yesterday.strftime('%Y年%m月%d日')
-    @brother_total = Brother.count
-    @entry_total   = Entry.count
-    @circus_total  = Circus.count
-    mail(to:      ["yuta.kurotaki@gmail.com", "k@u-n.jp"],
-         # cc:      [""],
+    staff_mail_list = YAML.load_file(Rails.root.to_s + '/config/staff_mail.yml')
+    @date           = Time.now.yesterday.strftime('%Y年%m月%d日')
+    @brother_total  = Brother.count
+    @entry_total    = Entry.count
+    @circus_total   = Circus.count
+    mail(to: staff_mail_list,
          subject: "【まめぶろ】Daily スコアレポート #{@date}")
   end
 end
