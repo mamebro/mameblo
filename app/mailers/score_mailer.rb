e16cc6b26ec2138171449faf32e289b21fbea103
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-09-11 05:48:58 +0900

    スコアに前日比を出すようにした

--- app/mailers/score_mailer.rb
+++ app/mailers/score_mailer.rb
@@ -3,10 +3,20 @@ class ScoreMailer < ActionMailer::Base
 
   def daily
     staff_mail_list = YAML.load_file(Rails.root.to_s + '/config/staff_mail.yml')
-    @date           = Time.now.yesterday.strftime('%Y年%m月%d日')
-    @brother_total  = Brother.count
-    @entry_total    = Entry.count
-    @circus_total   = Circus.count
+    yesterday       = Time.now.yesterday.all_day
+    day_before_yesterday = Time.now.yesterday.yesterday.all_day
+
+    @brother_yesterday_count = Brother.where(created_at: yesterday).count
+    @brother_day_before_yesterday = Brother.where(created_at: day_before_yesterday).count
+    @brother_diff = @brother_day_before_yesterday - @brother_yesterday_count
+    @brother_total = Brother.where("created_at <= ?" ,Time.now.yesterday.end_of_day).count
+
+    @entry_yesterday_count = Entry.where(created_at: yesterday).count
+    @entry_day_before_yesterday = Entry.where(created_at: day_before_yesterday).count
+    @entry_diff = @entry_day_before_yesterday - @entry_yesterday_count
+    @entry_total = Entry.where("created_at <= ?" ,Time.now.yesterday.end_of_day).count
+    @date = Time.now.yesterday.strftime('%Y年%m月%d日')
+
     mail(to: staff_mail_list,
          subject: "【まめぶろ】Daily スコアレポート #{@date}")
   end
