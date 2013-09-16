be56c8a0b80919cc6e261d18a16d2464ab223e62
shikakun <k@u-n.jp>
2013-09-16 13:07:12 +0900

    JavaScript を整理した

--- app/assets/javascripts/js.js
+++ app/assets/javascripts/js.js
@@ -12,12 +12,6 @@ $(document).ready(function() {
 	  }
   });
   
-//  $('#brothersflash').click(function(){
-//	  $('#brothersflash').hide();
-//    $('header nav').slideDown('fast');
-//  swNav = 1;
-//  });
-  
   $('#beBrother').click(function(){
     $('#beBrother').hide();
     $('#beBrotherAfter').show();
@@ -43,29 +37,19 @@ $(document).ready(function() {
   
   // textarea resize
   $('textarea').each(function(){
-	$(this)
-	    .css({
-		'overflow':'hidden',
-		'resize':'none'
-	    })
-	    .data('original_row',$(this).attr('rows'));
-    });
-    $('textarea').bind('keyup',function(){
-	var self = this;
-	var value = $(this).val().split("\n");
-	var value_row = 0;
-	$.each(value,function(i,val){
+	  $(this).css({'overflow': 'hidden', 'resize':'none'}).data('original_row',$(this).attr('rows'));
+  });
+  
+  $('textarea').bind('keyup',function(){
+	  var self = this;
+	  var value = $(this).val().split("\n");
+	  var value_row = 0;
+	  $.each(value,function(i,val){
 	    value_row += Math.max(Math.ceil(val.length/self.cols),1);
-	});
-	console.log(value_row);
-	var input_row = $(this).attr('rows');
-	var original_row = $(this).data('original_row');
-	var next_row = (input_row <= value_row) ? value_row+1 : Math.max(value_row+1,original_row);
-	$(this).attr('rows',next_row);
-    });
+	  });
+	  var input_row = $(this).attr('rows');
+	  var original_row = $(this).data('original_row');
+	  var next_row = (input_row <= value_row) ? value_row+1 : Math.max(value_row+1,original_row);
+	  $(this).attr('rows',next_row);
+  });
 });
-
-function test(a) {
-  if(a == null){ a = "alert!" }
-  window.alert(a);
-}
