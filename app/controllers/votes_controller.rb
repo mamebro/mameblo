145658d5e7ac327a83776674ddbb8a9a0e3d73e9
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-09-16 23:05:02 +0900

    投票できるようにした。投票済みの場合は、着たい!!!ボタンを表示しない

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,29 @@
 class VotesController < ApplicationController
+  before_action :signed_in_brother, only: [:index, :create]
+
   def index
+    @num = rand(11) + 1
+    @voted = if Vote.exists?(brother_id: current_brother.id, tshirt_id: @num)
+               true
+             else
+               false
+             end
   end
 
   def create
-    redirect_to votes_path
+    if Vote.exists?(brother_id: current_brother.id, tshirt_id: params[:tshirt_id])
+      redirect_to votes_path
+    else
+      Vote.create(brother_id: current_brother.id, tshirt_id: params[:tshirt_id], score: 1)
+      redirect_to votes_path
+    end
+  end
+
+  private
+  def signed_in_brother
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "!!! サインインしてちょ !!!" unless signed_in?
+    end
   end
 end
