68aaf744134435a7fb72d56906821744af5fcaa1
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-09-24 22:55:04 +0900

    選挙が終わったので index アクションだけにする

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,35 +1,8 @@
 class VotesController < ApplicationController
   include Ikachan
-  before_action :signed_in_brother, only: [:index, :create]
+  before_action :signed_in_brother, only: [:index]
 
   def index
-    @num = rand(12) + 1
-    @voted = if Vote.exists?(brother_id: current_brother.id, tshirt_id: @num)
-               true
-             else
-               false
-             end
-
-    vote_results = Vote.group(:tshirt_id).count
-    tshirt_id    = vote_results.keys.map {|i| "tshirt No." + i.to_s}
-    vote_scores  = vote_results.values
-
-    @bar = LazyHighCharts::HighChart.new('column') do |f|
-      f.title(text: '投票結果')
-      f.series(name: '投票数', data: vote_scores)
-      f.xAxis(categories: tshirt_id)
-      f.options[:chart][:defaultSeriesType] = "column"
-    end
-  end
-
-  def create
-    if Vote.exists?(brother_id: current_brother.id, tshirt_id: params[:tshirt_id])
-      redirect_to votes_path
-    else
-      Vote.create(brother_id: current_brother.id, tshirt_id: params[:tshirt_id], score: 1)
-      ikachan_post "誰かが #{params[:tshirt_id]} のTシャツに投票したよ!!!" if Rails.env.production?
-      redirect_to votes_path
-    end
   end
 
   private
