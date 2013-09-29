cbde87dd161f1df36c2b521dd3edf43d16507899
shikakun <k@u-n.jp>
2013-09-29 12:19:50 +0900

    textarea に関する JavaScript を整理した

--- app/assets/javascripts/textarea.js
+++ app/assets/javascripts/textarea.js
@@ -1,19 +1,19 @@
 $(function() {
-  $('textarea').each(function(){
-	  $(this).css({'overflow': 'hidden', 'resize':'none'}).data('original_row',$(this).attr('rows'));
+  $('textarea').each(function() {
+	  $(this).attr('data-rows-original', $(this).attr('rows'));
   });
   
-  $('textarea').bind('keyup',function(){
+  $('textarea').bind('keyup', function() {
 	  var self = this;
 	  var value = $(this).val().split("\n");
 	  var value_row = 0;
-	  $.each(value,function(i,val){
-	    value_row += Math.max(Math.ceil(val.length/self.cols),1);
+	  $.each(value, function(i, val) {
+	    value_row += Math.max(Math.ceil(val.length/self.cols), 1);
 	  });
 	  var input_row = $(this).attr('rows');
-	  var original_row = $(this).data('original_row');
-	  var next_row = (input_row <= value_row) ? value_row+1 : Math.max(value_row+1,original_row);
-	  $(this).attr('rows',next_row);
+	  var original_row = $(this).attr('data-rows-original');
+	  var next_row = (input_row <= value_row) ? value_row + 1 : Math.max(value_row + 1, original_row);
+	  $(this).attr('rows', next_row);
   });
 
   $('textarea').focus(function () {
