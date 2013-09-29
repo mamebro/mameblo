aff602889849195a6f03dbd74e24659c406c6ee6
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-09-29 21:57:50 +0900

    Fix deprecation warning: secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mameblog::Application.config.secret_token = '057a6e6292db82e788b924d926d0c4004ec09717d8303b51df8284cd988ac9daec9dc815b6ed15f6c15c58ada41b21e10300004d43951d5b218e4c3e30b93c49'
+Mameblog::Application.config.secret_key_base = '057a6e6292db82e788b924d926d0c4004ec09717d8303b51df8284cd988ac9daec9dc815b6ed15f6c15c58ada41b21e10300004d43951d5b218e4c3e30b93c49'
