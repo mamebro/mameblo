5fb2f140dcb4a13fee1bb5e2bcd7875dd04a839c
shikakun <k@u-n.jp>
2013-09-29 12:11:05 +0900

    textarea に関する JavaScript だけ切り出して別のファイルにした

--- app/assets/javascripts/js.js
+++ app/assets/javascripts/js.js
@@ -49,30 +49,4 @@ $(function() {
 			}
 		}
 	});
-  
-  // textarea resize
-  $('textarea').each(function(){
-	  $(this).css({'overflow': 'hidden', 'resize':'none'}).data('original_row',$(this).attr('rows'));
-  });
-  
-  $('textarea').bind('keyup',function(){
-	  var self = this;
-	  var value = $(this).val().split("\n");
-	  var value_row = 0;
-	  $.each(value,function(i,val){
-	    value_row += Math.max(Math.ceil(val.length/self.cols),1);
-	  });
-	  var input_row = $(this).attr('rows');
-	  var original_row = $(this).data('original_row');
-	  var next_row = (input_row <= value_row) ? value_row+1 : Math.max(value_row+1,original_row);
-	  $(this).attr('rows',next_row);
-  });
-
-  $('textarea').focus(function () {
-    $(this).addClass('focus');
-  });
-
-  $('#entry_form .input').focus(function () {
-    $('#entry-form-submit').show();
-  });
 });
