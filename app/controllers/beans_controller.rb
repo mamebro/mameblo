bce7815ee071ec473057061b55fd7c2d368ce659
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2013-12-02 00:31:37 +0900

    beans#create テンプレートは表示しないように指定

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -2,8 +2,7 @@ class BeansController < ApplicationController
 
   def create
     Beans.create(entry_id: params[:entry_id], throw_brother_id: current_brother.id)
-    @beans = Beans.where(entry_id: params[:entry_id]);
-    redirect_to entry_path
+    render nothing: true
   end
 
   def destroy
