63b0e6509f47cf0aa12e2371f2efc0afc98e40d6
shikakun <k@u-n.jp>
2013-12-31 21:32:28 +0900

    まめくじなので吉を豆に置換した

--- app/controllers/jinja/omikuji_controller.rb
+++ app/controllers/jinja/omikuji_controller.rb
@@ -1,6 +1,6 @@
 class Jinja::OmikujiController < ApplicationController
   def index
-    fortunes = ['大吉', '中吉', '小吉', '吉', '末吉']
+    fortunes = ['大豆', '中豆', '小豆', '豆', '末豆']
     beans = ['大豆', '小豆', '空豆', '仙豆', 'ひよこ豆', 'コーヒー豆', 'いんげん豆']
     @bean = beans.sample
     @fortune = fortunes.sample
