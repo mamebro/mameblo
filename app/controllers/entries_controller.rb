f4d0aa2e425b9b1992d989da9d76f6202767aad9
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-01-03 20:38:51 +0900

    エントリーのタイトルが空の時は今日の日付を入れる

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -20,6 +20,7 @@ include Ikachan
 
   def create
     @entry = current_brother.entries.build entry_params
+    @entry.title = Date.today.strftime("%Y/%m/%d") if @entry.title.empty?
     if @entry.save
       flash[:success] = "!!! ぶろぐ投稿できたね !!!"
       ikachan_post "#{current_brother.name} が投稿したよ! #{request.url}/#{@entry.id.to_s}" if Rails.env.production?
