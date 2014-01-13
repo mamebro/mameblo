51fef1123e1ecc04c8f06498c3089f7c5cca026c
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-01-13 20:49:02 +0900

    secure_token を動的に生成するようにした
    see: http://railstutorial.jp/chapters/static-pages?version=4.0#top

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,19 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mameblog::Application.config.secret_key_base = '057a6e6292db82e788b924d926d0c4004ec09717d8303b51df8284cd988ac9daec9dc815b6ed15f6c15c58ada41b21e10300004d43951d5b218e4c3e30b93c49'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Mameblog::Application.config.secret_key_base = secure_token
