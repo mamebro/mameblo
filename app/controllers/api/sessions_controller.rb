410d9b6a8b9ba79846fc3322987a4b38616fa28f
Keisuke KITA <kei.kita2501@gmail.com>
2014-02-23 15:07:26 +0900

    不正なパスワードのとき401がステータスコードになっていることを確かめるテストを書いた

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,7 +6,7 @@ module Api
       brother = Brother.find_by_name(params[:name])
       if brother && brother.authenticate(params[:password])
         authentication = brother.api_authentications.first || brother.api_authentications.create
-        render text: authentication.token, status: 200
+        render text: authentication.token, status: 200, content_type: Mime::TEXT
       else
         render nothing: true, status: 401
       end
--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -6,15 +6,37 @@ describe Api::SessionsController do
 
   describe 'create' do
     context 'success' do
-      it 'status 200' do
+      before do
         post :create, {name: brother.name, password: password}
+      end
+
+      it 'status 200' do
         expect(response.status).to eq(200)
+      end
+
+      it 'content-type text/plain' do
+        expect(response.content_type).to eq('text/plain')
+      end
+
+      it 'トークンが取得できる' do
+        expect(response.body).to match(/\A[0-9a-z]+\Z/)
+      end
+
+      context '複数回取得するとき' do
+        it '同じトークンが得られる' do
+          first_token = response.body
+          post :create, {name: brother.name, password: password}
+          expect(response.body).to eq first_token
+        end
       end
     end
 
     context 'fail' do
-      it 'status 401' do
+      let(:wrong_password) { 'nejimakidori' }
 
+      it 'status 401' do
+        post :create, {name: brother.name, password: wrong_password}
+        expect(response.status).to eq(401)
       end
     end
   end
@@ -36,5 +58,4 @@ describe Api::SessionsController do
       end
     end
   end
-
 end
