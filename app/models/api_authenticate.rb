b02f331e6ad478ba3a2ff52dd76e8e7f66c6ac72
Keisuke KITA <kei.kita2501@gmail.com>
2014-02-23 14:24:22 +0900

    重複したトークンを生成しないようにした

--- app/models/api_authenticate.rb
+++ app/models/api_authenticate.rb
@@ -2,6 +2,12 @@ class ApiAuthenticate < ActiveRecord::Base
   belongs_to :brother
 
   before_save do
-    self.token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64.to_s)
+    token = ''
+    begin
+      token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64.to_s)
+      authenticate = ApiAuthenticate.find_by token: token
+    end while authenticate.present?
+
+    self.token = token
   end
 end
