b820543bc33b915a15d5b0231814a9644e012666
Keisuke KITA <kei.kita2501@gmail.com>
2014-02-23 19:47:33 +0900

    db:migrateタスクを実行した

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140223053805) do
+ActiveRecord::Schema.define(version: 20140223104227) do
 
-  create_table "api_authentications", force: true do |t|
+  create_table "authentications", force: true do |t|
     t.integer  "brother_id"
     t.string   "token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "api_authentications", ["brother_id"], name: "index_api_authentications_on_brother_id"
-  add_index "api_authentications", ["token"], name: "index_api_authentications_on_token"
+  add_index "authentications", ["brother_id"], name: "index_authentications_on_brother_id"
+  add_index "authentications", ["token"], name: "index_authentications_on_token"
 
   create_table "beans", force: true do |t|
     t.integer  "kind",             default: 0
