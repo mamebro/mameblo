57e887814d10d2839dafbe3b9c5df684859b0caa
Keisuke KITA <kei.kita2501@gmail.com>
2014-02-23 14:07:15 +0900

    mamebloではユーザーはuserではなくbrotherだった

--- db/migrate/20140223045121_create_api_authenticates.rb
+++ db/migrate/20140223045121_create_api_authenticates.rb
@@ -1,13 +1,13 @@
 class CreateApiAuthenticates < ActiveRecord::Migration
   def change
     create_table :api_authenticates do |t|
-      t.integer :user_id
+      t.integer :brother_id
       t.string  :token
 
       t.timestamps
     end
 
-    add_index :api_authenticates, :user_id
+    add_index :api_authenticates, :brother_id
     add_index :api_authenticates, :token
   end
 end
