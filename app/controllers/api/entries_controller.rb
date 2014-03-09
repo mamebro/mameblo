f3b32aabab8c4e1815219bf390d716fe994b38af
Keisuke KITA <kei.kita2501@gmail.com>
2014-03-09 22:31:46 +0900

    EntryのAPI用のコントローラをつくった

---
+++ app/controllers/api/entries_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  class EntriesController < Api::ApplicationController
+    before_action :authenticate
+
+    def show
+      @entry = Entry.find(params[:id])
+      render json: @entry
+    end
+
+    def home
+    end
+  end
+end
