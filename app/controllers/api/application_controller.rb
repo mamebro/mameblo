248437bb488d58cfe14a60ed44e0cde0eda14137
Keisuke KITA <kei.kita2501@gmail.com>
2014-03-16 16:10:17 +0900

    homeアクションを書いた

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -3,16 +3,21 @@ module Api
     protect_from_forgery with: :null_session
     before_action :set_authentication
     before_action :authenticate
+    before_action :current_brother
 
     private
+    def set_authentication
+      @authentication = Authentication.find_by token: params[:auth_token]
+    end
+
     def authenticate
       unless @authentication && @authentication.brother.name == params[:name]
         render nothing: true, status: 401
       end
     end
 
-    def set_authentication
-      @authentication = Authentication.find_by token: params[:auth_token]
+    def current_brother
+      @authentication.try(:brother)
     end
   end
 end
--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -6,6 +6,19 @@ module Api
     end
 
     def home
+      followee = current_brother.followed_brothers
+      entries =
+        Entry.where(brother_id: followee)
+             .where("id > ?", params[:since_id] || 0)
+             .where("id <= ?", params[:max_id] || Entry.last.id)
+             .limit(params[:count] || 20)
+
+      render json: entries, meta: {
+        _links: {
+          latest: home_entries_url(since_id: entries.first.id),
+          previous: home_entries_url(max_id: entries.last.id-1)
+        }
+      }
     end
   end
 end
