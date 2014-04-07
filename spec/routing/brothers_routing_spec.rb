7d574729a4ae49f81d5e28b719eb93880d41ecee
kurotaky <yuta.kurotaki@gmail.com>
2014-04-08 05:47:44 +0900

    fix routing spec * https://github.com/rspec/rspec-rails#route_to

--- spec/routing/brothers_routing_spec.rb
+++ spec/routing/brothers_routing_spec.rb
@@ -3,8 +3,7 @@ require "spec_helper"
 describe BrothersController do
   describe "routing" do
     it "routes to #create" do
-      expect({post: "/brohters"}).to
-      route_to(controller: "brothers", action: "create")
+      expect(post: "/brothers").to route_to(controller: "brothers", action: "create")
     end
   end
 end
