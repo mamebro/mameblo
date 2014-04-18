bf53f232abc4afecf7193b7be53133bfdeff6a69
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-04-19 08:59:17 +0900

    文言の変更とupdateに変更

--- app/controllers/brothers_controller.rb
+++ app/controllers/brothers_controller.rb
@@ -39,8 +39,8 @@ include Ikachan
   end
 
   def update
-    if @brother.update_attributes brother_params
-      flash[:success] = "!!! プロフィール更新しました !!!"
+    if @brother.update brother_params
+      flash[:success] = "!!! パスワード変更しました !!!"
       sign_in @brother
       redirect_to @brother
     else
