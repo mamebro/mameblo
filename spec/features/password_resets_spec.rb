10fab53444c3f9adb1730ecc78c6bc1326b7660f
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-04-19 16:47:11 +0900

    テスト追加

---
+++ spec/features/password_resets_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature 'password resets' do
+
+  subject { page }
+
+  feature 'パスワード再設定画面へ移動' do
+    let(:brother) { FactoryGirl.create(:brother) }
+
+    before do
+      visit root_path
+      first(:link, 'サインイン').click
+      click_link 'パスワードを忘れちゃった..!'
+    end
+
+    scenario 'メールアドレスを入力する画面が表示される' do
+      is_expected.to have_content 'パスワードの再設定'
+    end
+  end
+end
