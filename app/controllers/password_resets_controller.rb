13fe12dbb03c6bbd46ba82db23aca1fac61c5744
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-04-19 13:53:10 +0900

    update アクションを追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,4 +11,24 @@ class PasswordResetsController < ApplicationController
   def edit
     @brother = Brother.find_by!(password_reset_token: params[:id])
   end
+
+  def update
+    @brother = Brother.find_by!(password_reset_token: params[:id])
+    if @brother.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, alert: "!!! 有効期限切れです !!!"
+    elsif @brother.update(brother_params)
+      redirect_to root_url, notice: "!!! パスワード再設定できました !!!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def brother_params
+    params.require(:brother).permit(:name,
+                                    :email,
+                                    :password,
+                                    :password_confirmation)
+  end
 end
