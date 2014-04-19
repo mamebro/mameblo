89f8616d5d6657264e83ce4696f784f052ab0495
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-04-19 10:57:41 +0900

    パスワード変更のためのメソッド追加

--- app/models/brother.rb
+++ app/models/brother.rb
@@ -57,9 +57,21 @@ class Brother < ActiveRecord::Base
     find_by_name(arg) || find(arg)
   end
 
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save! UserMailer.password_reset(self).deliver
+  end
+
   private
 
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
+  end
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column: self[column])
   end
 end
