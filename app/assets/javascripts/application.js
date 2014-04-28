df7263df2fad8a75a6a91456cb2852ca231842c3
shikakun <k@u-n.jp>
2014-04-28 22:30:16 +0900

    jquery は 1.x 系の最新を使用したいのでバージョンの指定を変更した https://github.com/mamebro/mameblo/pull/155#discussion-diff-12035034

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-//= require jquery/jquery
+//= require jquery/dist/jquery
 //= require jquery-touchswipe/jquery.touchSwipe
 //= require infinite-scroll/jquery.infinitescroll
 //= require emojify/emojify
