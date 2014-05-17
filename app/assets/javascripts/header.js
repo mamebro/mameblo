b5e6e1e8f852ba229490f15d7d11eeb544fdf8ac
shikakun <k@u-n.jp>
2014-05-17 22:17:14 +0900

    スマホのメニューの項目が順番に開くようにした

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,13 +1,27 @@
 $(function() {
   $('.nav-menu-control').click(function() {
     var $body = $('body'),
-        $navMenuContent = $('.nav-menu-content');
+        $navMenuContent = $('.nav-menu-content'),
+        $nav = $navMenuContent.find('.nav'),
+        animationDelay = 50;
     if ($body.hasClass('is-menu')) {
       $body.removeClass('is-menu');
       $navMenuContent.hide();
     } else {
       $body.addClass('is-menu');
       $navMenuContent.show();
+      $nav.each(function(index) {
+        $(this)
+          .animate({
+            'opacity': 0,
+            'margin-top': '20px'
+          }, 0)
+          .delay(animationDelay * index)
+          .animate({
+            'opacity': 1,
+            'margin-top': '0'
+          }, 100, 'swing');
+      });
     }
   });
 
