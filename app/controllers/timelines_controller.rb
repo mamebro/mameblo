64236770d59a716c4b15abfce294f06a9db9ac33
shikakun <k@u-n.jp>
2014-05-21 22:33:46 +0900

    トップページには自分を除くブラザー全員の投稿が一覧で表示されるように変更した

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,11 +2,7 @@ class TimelinesController < ApplicationController
   def index
     if signed_in?
       @entry  = current_brother.entries.build
-      followed_ids = []
-      Relationship.where(follower_id: current_brother.id).each do |rel|
-        followed_ids << rel.followed_id
-      end
-      @entries = Entry.where(brother_id: followed_ids).page params[:page]
+      @entries = Entry.where.not(brother_id: current_brother.id).page params[:page]
       @entries.each_with_index do |entry, index|
         @entries[index].content = entry.content_as_markdown
       end
