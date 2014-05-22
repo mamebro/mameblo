945b4253babb1125fd3f12949bcf4f59c62fee54
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-05-22 22:30:05 +0900

    view側でbuildしているので削除

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,6 @@
 class TimelinesController < ApplicationController
   def index
     if signed_in?
-      @entry  = current_brother.entries.build
       @entries = Entry.where.not(brother_id: current_brother.id).page params[:page]
     else
       @entries = Entry.page params[:page]
@@ -11,7 +10,6 @@ class TimelinesController < ApplicationController
 
   def brothers
     if signed_in?
-      @entry = current_brother.entries.build
       followed_ids = []
       Relationship.where(follower_id: current_brother.id).each do |rel|
         followed_ids << rel.followed_id
