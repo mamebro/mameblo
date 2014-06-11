5ac34db3b23a573580181a68a7afe51808b9259e
shikakun <k@u-n.jp>
2014-06-11 09:43:15 +0900

    豆投げについての JavaScript を別のファイルに切り出した

--- app/assets/javascripts/js.js
+++ app/assets/javascripts/js.js
@@ -35,48 +35,6 @@ $(function() {
     $('#beBrotherAfter').show();
   });
 
-  $('.entry-beans.is_throwable').swipe( {
-    swipeLeft:function(event, direction, distance, duration, fingerCount) {
-      $(this).submit();
-    },
-    threshold: 5
-  });
-
-  $('.entry-beans.is_throwable').submit(function() {
-    $(this)
-      .append('<span class="bean"></span>');
-    $(this).find('.bean:last-child')
-      .addClass(setBeanType())
-      .animate(
-        {
-          'margin-left': '125px',
-          'opacity': 0
-        },
-        0
-      )
-      .animate(
-        {
-          'margin-left': '0',
-          'opacity': 1
-        },
-        250,
-        'swing'
-      );
-    var $count = $(this).prev('.entry').find('.beans-count');
-    var currentBeans = Number($count.attr('data-beans-count')) + 1;
-    $count.html(currentBeans).attr('data-beans-count', currentBeans);
-  });
-
-  $('.bean').each(function () {
-    $(this).addClass(setBeanType());
-  });
-
-  function setBeanType() {
-    var beanTypes = ['bean-a', 'bean-b', 'bean-c', 'bean-d'];
-    var rand = 0 + Math.floor(Math.random() * 4);
-    return beanTypes[rand];
-  }
-
   // anchor
   $("a[href*=#]").click(function(){
     if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
