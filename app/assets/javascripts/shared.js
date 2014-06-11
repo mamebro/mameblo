ac15ae7558be4bb057d22aa0be18c5236a32b923
shikakun <k@u-n.jp>
2014-06-11 13:53:57 +0900

    豆の数をタップして豆を投げられるようにした

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -6,38 +6,13 @@ $(function() {
     threshold: 5
   });
 
+  $('.entry-beans.is_throwable').click(function() {
+    $(this).submit();
+  });
+
   $('.entry-beans.is_throwable').submit(function() {
-    $(this)
-      .append('<span class="bean"></span>');
-    $(this).find('.bean:last-child')
-      .addClass(setBeanType())
-      .animate(
-        {
-          'margin-left': '125px',
-          'opacity': 0
-        },
-        0
-      )
-      .animate(
-        {
-          'margin-left': '0',
-          'opacity': 1
-        },
-        250,
-        'swing'
-      );
-    var $count = $(this).prev('.entry').find('.beans-count');
+    var $count = $(this).find('.beans-count');
     var currentBeans = Number($count.attr('data-beans-count')) + 1;
     $count.html(currentBeans).attr('data-beans-count', currentBeans);
   });
-
-  $('.bean').each(function () {
-    $(this).addClass(setBeanType());
-  });
-
-  function setBeanType() {
-    var beanTypes = ['bean-a', 'bean-b', 'bean-c', 'bean-d'];
-    var rand = 0 + Math.floor(Math.random() * 4);
-    return beanTypes[rand];
-  }
 });
