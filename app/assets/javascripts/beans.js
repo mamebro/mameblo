30e6f07438e8ccde9784fc22f5963660fb3efcbe
shikakun <k@u-n.jp>
2014-06-12 09:41:58 +0900

    豆を投げたら豆が画面を横切るようにした

--- app/assets/javascripts/beans.js
+++ app/assets/javascripts/beans.js
@@ -8,12 +8,23 @@ $(function() {
 
   $('.entry-beans.is_throwable').click(function() {
     $(this).submit();
-    var beanFlyingDistance = window.innerWidth + 18,
+    var beanFlyingDistance = window.innerWidth,
         beanFlyingScope = {
           'start' : window.pageYOffset,
           'end'   : window.pageYOffset + window.innerHeight
         },
         beanFlyingPosition = beanFlyingScope['start'] + Math.floor(Math.random() * (beanFlyingScope['end'] - beanFlyingScope['start']));
+    $('body')
+      .append('<span class="bean" />')
+      .find('.bean:last-child')
+      .css('top', beanFlyingPosition)
+      .animate(
+        {
+          right: beanFlyingDistance
+        },
+        1000,
+        'linear'
+      );
   });
 
   $('.entry-beans.is_throwable').submit(function() {
