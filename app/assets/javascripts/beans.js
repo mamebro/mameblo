b50453194e18b79b97e644a8717c2f7d39e6866b
shikakun <k@u-n.jp>
2014-06-12 13:36:03 +0900

    オートもっとで動的に生成した日記にも豆を投げられるようにした

--- app/assets/javascripts/beans.js
+++ app/assets/javascripts/beans.js
@@ -6,31 +6,31 @@ $(function() {
     threshold: 5
   });
 
-  $('.entry-beans.is_throwable').click(function() {
-    $(this).submit();
-  });
-
-  $('.entry-beans.is_throwable').submit(function() {
-    var $entry = $(this).parents('.entry'),
-        currentBeans = Number($entry.attr('data-beans-count')) + 1,
-        beanFlyingDistance = window.innerWidth,
-        beanFlyingScope = {
-          'start' : window.pageYOffset,
-          'end'   : window.pageYOffset + window.innerHeight
-        },
-        beanFlyingPosition = beanFlyingScope['start'] + Math.floor(Math.random() * (beanFlyingScope['end'] - beanFlyingScope['start']));
-    $entry.attr('data-beans-count', currentBeans);
-    $(this).find('.beans-count').html(currentBeans);
-    $('body')
-      .append('<span class="bean" />')
-      .find('.bean:last-child')
-      .css('top', beanFlyingPosition)
-      .animate(
-        {
-          right: beanFlyingDistance
-        },
-        1000,
-        'linear'
-      );
-  });
+  $(document.documentElement)
+    .on('click', '.entry-beans.is_throwable', function(e) {
+      $(this).submit();
+    })
+    .on('submit', '.entry-beans.is_throwable', function(e) {
+      var $entry = $(this).parents('.entry'),
+          currentBeans = Number($entry.attr('data-beans-count')) + 1,
+          beanFlyingDistance = window.innerWidth,
+          beanFlyingScope = {
+            'start' : window.pageYOffset,
+            'end'   : window.pageYOffset + window.innerHeight
+          },
+          beanFlyingPosition = beanFlyingScope['start'] + Math.floor(Math.random() * (beanFlyingScope['end'] - beanFlyingScope['start']));
+      $entry.attr('data-beans-count', currentBeans);
+      $(this).find('.beans-count').html(currentBeans);
+      $('body')
+        .append('<span class="bean" />')
+        .find('.bean:last-child')
+        .css('top', beanFlyingPosition)
+        .animate(
+          {
+            right: beanFlyingDistance
+          },
+          1000,
+          'linear'
+        );
+    });
 });
