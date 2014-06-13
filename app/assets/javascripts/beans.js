add95a04fec9ea33172f09930c39a954376a8ec3
shikakun <k@u-n.jp>
2014-06-13 09:20:01 +0900

    豆を投げるアニメーションを関数に切り出した

--- app/assets/javascripts/beans.js
+++ app/assets/javascripts/beans.js
@@ -3,32 +3,41 @@ $(function() {
 
   $(document.documentElement)
     .on('click', '.entry-beans.is_throwable', function(e) {
+      throwOmnidirectionalBean();
       $(this).submit();
     })
     .on('submit', '.entry-beans.is_throwable', function(e) {
       var $entry = $(this).parents('.entry'),
-          currentBeans = Number($entry.attr('data-beans-count')) + 1,
-          beanFlyingDistance = window.innerWidth,
-          beanFlyingScope = {
-            'start' : window.pageYOffset,
-            'end'   : window.pageYOffset + window.innerHeight
-          },
-          beanFlyingPosition = beanFlyingScope['start'] + Math.floor(Math.random() * (beanFlyingScope['end'] - beanFlyingScope['start']));
+          currentBeans = Number($entry.attr('data-beans-count')) + 1;
       $entry.attr('data-beans-count', currentBeans);
       $(this).find('.beans-count').html(currentBeans);
-      $('body')
-        .append('<span class="bean" />')
-        .find('.bean:last-child')
-        .css('top', beanFlyingPosition)
-        .animate(
-          {
-            right: beanFlyingDistance
-          },
-          500,
-          'linear'
-        );
     })
     .on('touchstart', '.entry', function(e) {
       $(this).find('.entry-beans.is_throwable').submit();
     });
+
+  function throwOmnidirectionalBean() {
+    // 無指向性の豆
+    var beanFlyingDistance = window.innerWidth,
+        beanFlyingScope = {
+          'start' : window.pageYOffset,
+          'end'   : window.pageYOffset + window.innerHeight
+        },
+        beanFlyingPosition = beanFlyingScope['start'] + Math.floor(Math.random() * (beanFlyingScope['end'] - beanFlyingScope['start']));
+    $('body')
+      .append('<span class="bean" />')
+      .find('.bean:last-child')
+      .css('top', beanFlyingPosition)
+      .animate(
+        {
+          right: beanFlyingDistance
+        },
+        500,
+        'linear'
+      );
+  }
+
+  function throwUnidirectionalBean() {
+    // 指向性のある豆
+  }
 });
