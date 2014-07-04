316e1d433ba16c0b6d43b52d127decdcefd3e30b
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-07-04 14:53:12 +0900

    Revert "Merge pull request #200 from mamebro/draft"
    This reverts commit 1728817441912e0022377698e10bdb6640bb5870, reversing
    changes made to dc9b439d2e0db392e0890974455ae32842344b70.

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,53 +1,9 @@
 $(function() {
-  var $entryForm = $('#new_entry'),
-      $entryFormTitle = $entryForm.find('#entry-form-title'),
-      $entryFormContent = $entryForm.find('#entry-form-content');
-
-  $entryFormTitle
-    .val(localStorage.getItem('entryFormTitle'));
-  $entryFormContent
-    .val(localStorage.getItem('entryFormContent'))
-    .attr('data-rows-original', $entryFormContent.attr('rows'));
-
-  checkSubmittable();
-
-  $entryFormTitle.bind('keyup', function() {
-    // 入力した文字を保存する
-    localStorage.setItem('entryFormTitle', $(this).val());
-  });
-
-  $entryFormContent.bind('keyup', function() {
-    var inputText = $(this).val();
-
-    // 入力した文字数にあわせて textarea を広げる
-    var self = this;
-    var value = inputText.split('\n');
-    var value_row = 0;
-    $.each(value, function(i, val) {
-      value_row += Math.max(Math.ceil(val.length/self.cols), 1);
-    });
-    var input_row = $(this).attr('rows');
-    var original_row = $(this).attr('data-rows-original');
-    var next_row = (input_row <= value_row) ? value_row + 1 : Math.max(value_row + 1, original_row);
-    $(this).attr('rows', next_row);
-
-    // 入力した文字を保存する
-    localStorage.setItem('entryFormContent', inputText);
-
-    // 日記を投稿できるかどうか調べる
-    checkSubmittable();
-  });
-
-  $entryForm.submit(function() {
-    clearDraft();
-  });
-
   $('.nav-editor-control').click(function() {
     var $body = $('body');
     if ($body.hasClass('is-editor')) {
-      if ($entryForm.hasClass('submittable')) {
+      if ($('#new_entry').hasClass('submitable')) {
         if (window.confirm('投稿しなくて大丈夫？')) {
-          clearDraft();
           $body.removeClass('is-editor');
         }
       } else {
@@ -58,34 +14,43 @@ $(function() {
     }
   });
 
-  $entryForm.find('textarea').focus(function () {
+  $('#entry-form-content').attr('data-rows-original', $('#entry-form-content').attr('rows'));
+
+  $('#entry-form-content').bind('keyup', function() {
+	  var self = this;
+	  var value = $(this).val().split('\n');
+	  var value_row = 0;
+	  $.each(value, function(i, val) {
+	    value_row += Math.max(Math.ceil(val.length/self.cols), 1);
+	  });
+	  var input_row = $(this).attr('rows');
+	  var original_row = $(this).attr('data-rows-original');
+	  var next_row = (input_row <= value_row) ? value_row + 1 : Math.max(value_row + 1, original_row);
+	  $(this).attr('rows', next_row);
+    checkSubmitable();
+  });
+
+  $('.new_entry, .edit_entry').find('textarea').focus(function () {
     if (!$(this).hasClass('focus')) {
       $(this).addClass('focus');
     }
     $(window).keydown(function (e) {
       if (e.metaKey && e.keyCode === 13) {
-        $entryForm.submit();
+        $('.new_entry, .edit_entry').submit();
       }
     });
-    checkSubmittable();
+    checkSubmitable();
   });
 
-  function clearDraft() {
-    localStorage.clear('entryFormTitle');
-    localStorage.clear('entryFormContent');
-    $entryFormTitle.val('');
-    $entryFormContent.val('');
-  }
-
-  function checkSubmittable() {
-    if ($entryFormContent.val().replace(/\s/g, '').length > 0) {
-      if (!$entryForm.hasClass('submittable')) {
-        $entryForm.addClass('submittable');
+  function checkSubmitable() {
+    if ($('#entry-form-content').val().replace(/\s/g, '').length > 0) {
+      if (!$('#new_entry').hasClass('submitable')) {
+        $('#new_entry').addClass('submitable');
         $('#entry-form-submit').fadeIn(250);
       }
     } else {
-      if ($entryForm.hasClass('submittable')) {
-        $entryForm.removeClass('submittable');
+      if ($('#new_entry').hasClass('submitable')) {
+        $('#new_entry').removeClass('submitable');
         $('#entry-form-submit').fadeOut(250);
       }
     }
