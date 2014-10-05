93c2ceb813418059b32bd60a06ec19a7c42e8de6
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2014-10-05 16:07:34 +0900

    まめ投げた時のテストを修正

--- spec/controllers/beans_controller_spec.rb
+++ spec/controllers/beans_controller_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe BeansController, type: :controller do
-
-  pending "POST 'create'" do
+  describe "POST 'create'" do
     it "returns http success" do
+      bro = FactoryGirl.create(:brother)
+      sign_in bro
       expect do
         xhr :post, :create, entry_id: 1
-      end.to change(bean, :count).by(1)
-      response.should be_success
+      end.to change(Bean, :count).by(1)
+      expect(response).to be_success
     end
   end
 end
