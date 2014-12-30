139d3a378c1827e1735ad8d8a70d310526abf194
shikakun <k@u-n.jp>
2014-12-30 20:34:21 +0900

    よく考えたら前後に半角スペース入れて検索しなくてもいいのかなと思った

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -1,6 +1,6 @@
 class HashtagController < ApplicationController
   def show
-    @entries = Entry.where("content LIKE ?", "% ##{params[:id]} %")
+    @entries = Entry.where("content LIKE ?", "%##{params[:id]}%")
     @entries = @entries.page params[:page]
   end
 end
