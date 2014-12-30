b5ff5dff79e63a8cf0df9bee65c2ec69db95ba4b
shikakun <k@u-n.jp>
2014-12-30 20:40:16 +0900

    ハッシュタグページでも Markdown をパースして表示するようにした

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -2,5 +2,6 @@ class HashtagController < ApplicationController
   def show
     @entries = Entry.where("content LIKE ?", "%##{params[:id]}%")
     @entries = @entries.page params[:page]
+    @entries.map{|entry| entry.content = entry.content_as_markdown}
   end
 end
