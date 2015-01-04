d132218ffbc5764d0526fc40c6cabf9b59ddb15b
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2015-01-05 00:09:59 +0900

    ハッシュタグ検索結果はHashtagの名前でとってくる

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -1,7 +1,6 @@
 class HashtagController < ApplicationController
   def show
-    @entries = Entry.where("content LIKE ?", "%##{params[:id]}%")
-    @entries = @entries.page params[:page]
+    @entries = Hashtag.find_by(name: params[:id]).entries.page params[:page]
     @entries.map{|entry| entry.content = entry.content_as_markdown}
   end
 end
