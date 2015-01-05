ca9d2a6a0a12c81133839aabde71e56149969cbb
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2015-01-05 09:31:38 +0900

    編集のテストを追加

--- spec/features/hashtag_spec.rb
+++ spec/features/hashtag_spec.rb
@@ -6,7 +6,7 @@ feature 'ハッシュタグ検索' do
 
   before do
     sign_in brother
-    fill_in 'entry-form-title', with: 'テスト'
+    fill_in 'entry-form-title', with: 'test title'
     fill_in 'entry-form-content', with: 'テスト #test #foo'
     click_on '!!! 投稿 !!!'
   end
@@ -26,6 +26,17 @@ feature 'ハッシュタグ検索' do
     expect(has_content?('!!!foo!!!! #foo')).to be_falsey
   end
 
-  scenario '編集'
+  scenario '編集' do
+    expect{
+      click_link('test title')
+      click_link('Edit')
+      within('.edit_entry') do
+        fill_in 'entry-form-content', with: 'テスト #test'
+      end
+      click_on '!!! 編集 !!!'
+    }.to change{EntryHasHashtag.count}.by(-1)
+    expect(has_content?('テスト #test')).to be_truthy
+  end
+
   scenario '削除'
 end
