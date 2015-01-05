a8778ab34d69e8c9035ad1989195b3de0bad97bd
shikakun <k@u-n.jp>
2015-01-05 20:50:47 +0900

    ハッシュタグのページにハッシュタグの名前を表示した

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -1,6 +1,7 @@
 class HashtagController < ApplicationController
   def show
     if hashtag = Hashtag.find_by(name: params[:id])
+      @hashtag_name = hashtag.name
       @entries = hashtag.entries.page params[:page]
       @entries.map{|entry| entry.content = entry.content_as_markdown}
     else
