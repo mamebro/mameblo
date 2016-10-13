ae71b1b112a87af2d8a3a3f932c248962ee2e722
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-10-13 23:39:58 +0900

    Revert "fix deprecated error"
    This reverts commit 172801d711e00fb3241cc5026bd6817d0ed4ca20.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Mameblog::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.public_file_server.enabled  = true
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
